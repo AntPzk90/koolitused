b65af94cf8045e8cdb4ef7412ef30e02719374cd
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2020-09-09 15:10:17 +0300

    add index page

--- js/script.js
+++ js/script.js
@@ -1,111 +1,48 @@
 // === подключение библиотек === //
-//преобразуем селекты библиотека https://select2.org/
 let viewport = $(document).width();
-$(document).ready(function() {
-  $('.form__select').select2({
-  });
-  $('.pagination__select').select2({
-    minimumResultsForSearch: -1
-  });
-});
 // === главная страница === //
 // открывание мобильного меню
 $('.header__mobile-btn').click(function () {
   $('.header__navigation').slideToggle();
   if (viewport < 768) {
-    $('.header__user-list').slideToggle();
-    $('.header__user-list').css('display', 'flex');
-    $('.header__user').css('order', '1')
+    $('.header__user').fadeToggle();
+    $('.header__languages').fadeToggle().css('display', 'flex');
   }
-  if ($(this).hasClass('header__mobile-btn--gamburger')) {
-    $(this).removeClass('header__mobile-btn--gamburger');
-    $(this).addClass('header__mobile-btn--close')
-  } else if ($(this).hasClass('header__mobile-btn--close')) {
-    $(this).removeClass('header__mobile-btn--close');
-    $(this).addClass('header__mobile-btn--gamburger');
-    $('.header__user').removeAttr('style');
-    if (viewport < 768) {
-      $('.header__user-list').slideUp();
+});
+// появление фильтра
+$('.filters__detail').click(function() {
+  $(this).css('display', 'none');
+  $('.filters__form-middle-wrapper').fadeToggle().css('display', 'flex');
+  $('.filters__form-bottom-wrapper').fadeToggle().css('display', 'flex');
+});
+//  попап регистрации
+$('.header__user-link--registration').click(function() {
+  $('.registration').fadeIn();
+});
+
+$('.filters__detail-back').click(function() {
+  $('.filters__detail').fadeIn();
+  $('.filters__form-middle-wrapper').fadeToggle();
+  $('.filters__form-bottom-wrapper').fadeToggle();
+});
+
+$('.header__user-link--enter').click(function() {
+  $('.login').fadeIn();
+  $('.login__registration-btn').click(function() {
+    $(this).parent().fadeOut(100);
+    $('.registration').fadeIn(500);
+  })
+});
+
+$('.popup__btn').click(function() {
+  $(this).parent().fadeOut();
+});
+
+$(document).keydown(function(evt) {
+  console.log($(evt.keyCode))
+  if ($(evt.keyCode)[0] == 27) {
+    if ($('.popup')) {
+      $('.popup').fadeOut();
     }
   }
 });
-// открывание подменю в пункте услуги
-$('.header__sub-list-btn').click(function() {
-  $(this).toggleClass('header__sub-list-btn--active');
-  if ($(this).hasClass('header__sub-list-btn--active')) {
-    $('.header__sub-list').slideDown();
-  } else {
-    $('.header__sub-list').slideUp();
-  }
-});
-// открывание меню залогиненого юзера
-$('.header__logged-icon-img').click(function () {
-  $('.header__logged-name').slideToggle();
-  $('.header__logged-list').slideToggle();
-  $('.header__logged-icon').css('display', 'block');
-});
-
-// открывание фильтров на мобильной версии
-$('.present__form-filter-mobile-btn').click(function() {
-  $('.present__form').addClass('present__form--active');
-  $(this).css('display', 'none');
-  $('.present__form-wrapper').css('padding-bottom', '0');
-  $('html, body').animate({scrollTop: 0},300);
-});
-// закрытие фильтров на мобильной версии
-$('.present__form-btn-close').click(function() {
-  if ($('.present__form').hasClass('present__form--active')){
-    $('.present__form').removeClass('present__form--active');
-    $('.present__form-filter-mobile-btn').removeAttr('style');
-    $('.present__form-wrapper').removeAttr('style');
-  }
-});
-// slick slider на главной странице
-if (viewport < 1200) {
-  $('.present__slider-wrapper').slick({
-    responsive: [
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-          arrows: false,
-          rows: 1,
-        }
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: true,
-          arrows: false,
-          rows: 1
-        }
-      },
-    ]
-  });
-} else {
-  $('.present__slider-wrapper').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    rows: 2,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    accessibility: false
-  });
-}
-// появление подменю в пункте УСЛУГИ
-if (viewport > 1200) {
-  $('.header__navigation-list-item--services').mouseenter(function() {
-    console.log($('.header__sub-list'))
-    $('.header__sub-list').slideDown();
-  });
-  $('.header__navigation-list-item--services').mouseleave(function() {
-    console.log($('.header__sub-list'))
-    $('.header__sub-list').slideUp();
-  });
-}
